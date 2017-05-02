01a01d9458436f8a94ba065e468c3f129c99dbf4
webcat12345 <webcat91@gmail.com>
2017-05-02 15:44:28 +0200

    Implement Tags Input + Unit Testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { TagInputModule } from 'ng2-tag-input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TagInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
